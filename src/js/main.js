5dae3c5244a0fd2ed98367b0c7c9c37f5e88f7e5
jamesonamadeus <jamesonamadeus@gmail.com>
2018-03-25 10:17:52 -0500

    add in reveal

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
 //Change Navbar Background on Scroll
-window.onscroll = function () {
-    var bnr = document.querySelector('#banner');
-    // var newLogo = (document.getElementById('logo-img').src = '../images/boataoke_black.png');
+window.onscroll = function() {
+	var bnr = document.querySelector('#banner');
+	// var newLogo = (document.getElementById('logo-img').src = '../images/boataoke_black.png');
 
-    if (window.pageYOffset >= 50) {
-        bnr.classList.add('banner--scroll');
-        bnr.classList.remove('banner--top');
-    } else {
-        bnr.classList.add('banner--top');
-        bnr.classList.remove('banner--scroll');
-    }
+	if (window.pageYOffset >= 50) {
+		bnr.classList.add('banner--scroll');
+		bnr.classList.remove('banner--top');
+	} else {
+		bnr.classList.add('banner--top');
+		bnr.classList.remove('banner--scroll');
+	}
 };
+
+//Reveal
+window.sr = ScrollReveal();
+sr.reveal('h3', {
+	delay: 100,
+	duration: 400,
+	easing: 'linear',
+	origin: 'left'
+});
