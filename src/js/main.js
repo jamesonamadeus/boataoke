7a1395eba38eb61b14d672b056a53b04aef465cf
jamesonamadeus <jamesonamadeus@gmail.com>
2018-04-11 16:33:00 -0400

    update the navigation

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,24 @@ window.onscroll = function() {
 	}
 };
 
+//nav brgr
+(function() {
+	var body = document.body;
+	var burgerMenu = document.getElementsByClassName('nav-menu')[0];
+	var burgerContain = document.getElementsByClassName('nav-contain')[0];
+	var burgerNav = document.getElementsByClassName('nav-list')[0];
+
+	burgerMenu.addEventListener(
+		'click',
+		function toggleClasses() {
+			[body, burgerContain, burgerNav].forEach(function(el) {
+				el.classList.toggle('open');
+			});
+		},
+		false
+	);
+})();
+
 //Reveal
 window.sr = ScrollReveal();
 sr.reveal('h3', {
