b2fd4e4ddd4a54713f349bb8ed313f1d0c787b95
jamesonamadeus <jamesonamadeus@gmail.com>
2018-04-12 01:42:34 -0400

    update the gulp file for fonts

--- gulpfile.js
+++ gulpfile.js
@@ -9,64 +9,76 @@ var cache = require('gulp-cache');
 var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 
-
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'src'
-        },
-    })
-})
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		}
+	});
+});
 
 // sass
 gulp.task('sass', function() {
-    return gulp.src('src/css/scss/**/*.scss') // pulls all .scss files & its children
-      .pipe(sass()) // scss files --> css
-      .pipe(gulp.dest('src/css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }))
+	return gulp
+		.src('src/css/scss/**/*.scss') // pulls all .scss files & its children
+		.pipe(sass()) // scss files --> css
+		.pipe(gulp.dest('src/css'))
+		.pipe(
+			browserSync.reload({
+				stream: true
+			})
+		);
 });
 
 // watch
-gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch('src/css/scss/**/*.scss', ['sass']); 
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('src/*.html', browserSync.reload); 
-    gulp.watch('src/js/**/*.js', browserSync.reload); 
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('src/css/scss/**/*.scss', ['sass']);
+	// Reloads the browser whenever HTML or JS files change
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
-
 // minify
-gulp.task('useref', function(){
-    return gulp.src('src/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', uglify()))
-        // Minifies only if it's a CSS file
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('dist'))
+gulp.task('useref', function() {
+	return (
+		gulp
+			.src('src/*.html')
+			.pipe(useref())
+			.pipe(gulpIf('*.js', uglify()))
+			// Minifies only if it's a CSS file
+			.pipe(gulpIf('*.css', cssnano()))
+			.pipe(gulp.dest('dist'))
+	);
 });
 
 // images
-gulp.task('images', function(){
-    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-        interlaced: true
-      })))
-    .pipe(gulp.dest('dist/images'))
+gulp.task('images', function() {
+	return (
+		gulp
+			.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+			// Caching images that ran through imagemin
+			.pipe(
+				cache(
+					imagemin({
+						interlaced: true
+					})
+				)
+			)
+			.pipe(gulp.dest('dist/images'))
+	);
+});
+
+// fonts
+gulp.task('fonts', function() {
+	return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
 });
 
 // build
-gulp.task('build', function (callback) {
-    runSequence(['sass','useref', 'images'],
-      callback
-    )
-})
+gulp.task('build', function(callback) {
+	runSequence(['sass', 'useref', 'images', 'fonts'], callback);
+});
 
 //default
-gulp.task('default', function (callback) {
-    runSequence(['sass','browserSync', 'watch'],
-      callback
-    )
-})
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'browserSync', 'watch'], callback);
+});
